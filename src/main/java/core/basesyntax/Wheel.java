8007cc5995ecf7f0013c86dedcc3b3174c0b37ed
Mariia Makohin <maria.makohin2420@gmail.com>
2026-06-17 12:59:47 +0300

    first step

--- src/main/java/core/basesyntax/Wheel.java
+++ src/main/java/core/basesyntax/Wheel.java
@@ -1,9 +1,32 @@
 package core.basesyntax;
 
-public class Wheel {
+import java.util.Objects;
+
+public class Wheel implements Cloneable {
     private int radius;
 
-    //implement this class
+    public Wheel(int radius) {
+        this.radius = radius;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Wheel wheel = (Wheel) o;
+        return radius == wheel.radius;
+    }
+    public int hashCode() {
+        return Objects.hashCode(radius);
+    }
+    public Wheel clone() {
+        try {
+            return (Wheel) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't clone Wheel", e);
+        }
+    }
 
     @Override
     public String toString() {
